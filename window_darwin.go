5045b1ae42dc3293aaa8db2d2ed701cd3e2e3349
skylerlee <skyler.ac.lee@gmail.com>
2019-08-09 14:53:50 +0800

    feat: add darwin platform window

---
+++ window_darwin.go
@@ -0,0 +1,24 @@
+package gocef
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Cocoa
+#import <Cocoa/Cocoa.h>
+
+NSWindow* cocoa_window_new() {
+  NSRect wndRect = NSMakeRect(100, 100, 200, 100);
+  NSUInteger wndStyle = (NSTitledWindowMask | NSClosableWindowMask |
+                         NSResizableWindowMask | NSMiniaturizableWindowMask);
+  NSWindow* wnd = [[NSWindow alloc] initWithContentRect:wndRect
+                                    styleMask:wndStyle
+                                    backing:NSBackingStoreBuffered
+                                    defer:NO];
+  return wnd;
+}
+
+*/
+import "C"
+import "unsafe"
+
+func CreateWindow() unsafe.Pointer {
+}
