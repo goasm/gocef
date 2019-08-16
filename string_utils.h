57d7ed53ad9476fc1d6a1879f3d27e7ac94bcd39
skylerlee <skyler.ac.lee@gmail.com>
2019-08-16 13:06:42 +0800

    fix: fix c exporting issue

--- string_utils.h
+++ string_utils.h
@@ -5,7 +5,7 @@
 
 #include "include/internal/cef_string.h"
 
-int gocef_set_string_utf8(_GoString_ src, cef_string_utf8_t* out) {
+static int gocef_set_string_utf8(_GoString_ src, cef_string_utf8_t* out) {
   const char* str = _GoStringPtr(src);
   size_t length = _GoStringLen(src);
   out->str = (char*)str;
@@ -13,7 +13,7 @@ int gocef_set_string_utf8(_GoString_ src, cef_string_utf8_t* out) {
   return length;
 }
 
-int gocef_set_string_utf16(_GoString_ src, cef_string_utf16_t* out) {
+static int gocef_set_string_utf16(_GoString_ src, cef_string_utf16_t* out) {
   const char* str = _GoStringPtr(src);
   size_t length = _GoStringLen(src);
   return cef_string_utf8_to_utf16(str, length, out);
