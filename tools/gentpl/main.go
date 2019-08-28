81ae77968de888455ba290499e7d320850db5304
skylerlee <skyler.ac.lee@gmail.com>
2019-08-28 22:51:56 +0800

    feat: update tools

--- tools/gentpl/main.go
+++ tools/gentpl/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 type context struct {
-	platform string
+	Platform string
+	CEFBuild string
 }
 
 func getTargetPath(src string) string {
@@ -18,23 +19,28 @@ func getTargetPath(src string) string {
 	return fmt.Sprintf("%s_%s.go", basename, platform)
 }
 
-func renderTemplate(tpl *template.Template, dst string) {
+func renderTemplate(tpl *template.Template, dst string) error {
 	out, err := os.OpenFile(dst, os.O_WRONLY|os.O_TRUNC, 0)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer out.Close()
 	ctx := context{
-		runtime.GOOS,
+		Platform: runtime.GOOS,
+		CEFBuild: "cef",
 	}
 	err = tpl.Execute(out, ctx)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
 func main() {
 	filename := os.Args[1]
 	template := template.Must(template.ParseFiles(filename))
-	renderTemplate(template, getTargetPath(filename))
+	err := renderTemplate(template, getTargetPath(filename))
+	if err != nil {
+		panic(err)
+	}
 }
