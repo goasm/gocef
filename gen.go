9b48c015b11e9684ab114516c8dc649537a360dd
skylerlee <skyler.ac.lee@gmail.com>
2019-08-30 01:41:57 +0800

    feat: update gen file

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package gocef
 
-//go:generate go run ./tools/gentpl lib.gotpl
+//go:generate go run ./tools/gentpl -cef cef_binary_74.1.19 lib.gotpl
--- lib_linux.go
+++ lib_linux.go
@@ -1,7 +1,7 @@
 package gocef
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/vendor/cef
-#cgo LDFLAGS: -L${SRCDIR}/vendor/cef/Release -lcef
+#cgo CFLAGS: -I${SRCDIR}/vendor/cef_binary_74.1.19
+#cgo LDFLAGS: -L${SRCDIR}/vendor/cef_binary_74.1.19/Release -lcef
 */
 import "C"
