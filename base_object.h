56ab4139528da1a356afc57dd885dd731c6e63ea
skylerlee <skyler.ac.lee@gmail.com>
2019-08-31 15:02:38 +0800

    feat: add reference counting header

---
+++ base_object.h
@@ -0,0 +1,28 @@
+// gocef base object
+#ifndef GOCEF_BASE_OBJECT_H_
+#define GOCEF_BASE_OBJECT_H_
+#pragma once
+
+#include <stdlib.h>
+#include "include/capi/cef_base_capi.h"
+
+void add_ref_impl(cef_base_ref_counted_t* self) {
+}
+
+int release_impl(cef_base_ref_counted_t* self) {
+}
+
+int has_one_ref_impl(cef_base_ref_counted_t* self) {
+}
+
+static void* gocef_new(size_t size) {
+  void* obj = calloc(1, size);
+  cef_base_ref_counted_t* base = (cef_base_ref_counted_t*)obj;
+  base->size = size;
+  base->add_ref = add_ref_impl;
+  base->release = release_impl;
+  base->has_one_ref = has_one_ref_impl;
+  return obj;
+}
+
+#endif  // GOCEF_BASE_OBJECT_H_
