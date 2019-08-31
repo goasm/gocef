d484875753adeb78db971a8a5fc51e2cb54e03c8
skylerlee <skyler.ac.lee@gmail.com>
2019-09-01 05:13:53 +0800

    feat: add types declaration

---
+++ types.go
@@ -0,0 +1,47 @@
+package gocef
+
+import "image/color"
+
+type LogSeverity int
+
+const (
+	LogSeverityDefault LogSeverity = iota
+	LogSeverityVerbose
+	LogSeverityInfo
+	LogSeverityWarning
+	LogSeverityError
+	LogSeverityDisable LogSeverity = 99
+)
+
+type Settings struct {
+	SingleProcess               bool
+	NoSandbox                   bool
+	BrowserSubprocessPath       string
+	FrameworkDirPath            string
+	MultiThreadedMessageLoop    bool
+	ExternalMessagePump         bool
+	WindowlessRenderingEnabled  bool
+	CommandLineArgsDisabled     bool
+	CachePath                   string
+	UserDataPath                string
+	PersistSessionCookies       bool
+	PersistUserPreferences      bool
+	UserAgent                   string
+	ProductVersion              string
+	Locale                      string
+	LogFile                     string
+	LogSeverity                 LogSeverity
+	JavascriptFlags             string
+	ResourcesDirPath            string
+	LocalesDirPath              string
+	PackLoadingDisabled         bool
+	RemoteDebuggingPort         int
+	UncaughtExceptionStackSize  int
+	IgnoreCertificateErrors     bool
+	EnableNetSecurityExpiration bool
+	BackgroundColor             color.Color
+	AcceptLanguageList          string
+}
+
+func (s *Settings) toNative() {
+}
