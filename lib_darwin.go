04bb5a5541e2df1e04482469ebb3b2cb584aa3e1
skylerlee <skyler.ac.lee@gmail.com>
2019-09-02 21:27:28 +0800

    chore: update

--- lib_darwin.go
+++ lib_darwin.go
@@ -1,7 +1,7 @@
 package gocef
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/vendor/cef
-#cgo LDFLAGS: -F${SRCDIR}/vendor/cef/Release -framework "Chromium Embedded Framework"
+#cgo CFLAGS: -I${SRCDIR}/vendor/cef_binary_74.1.19
+#cgo LDFLAGS: -F${SRCDIR}/vendor/cef_binary_74.1.19/Release -framework "Chromium Embedded Framework"
 */
 import "C"
