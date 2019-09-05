b50a2534b690327d1411764878dd63041f2fc616
skylerlee <skyler.ac.lee@gmail.com>
2019-09-05 23:49:15 +0800

    refactor: merge C files to Go

--- type_conv.go
+++ type_conv.go
@@ -1,7 +1,22 @@
 package gocef
 
 /*
-#include "type_conv.h"
+#include <stdlib.h>
+#include "include/internal/cef_string.h"
+
+cef_string_utf8_t* gocef_to_utf8_impl(char* str, size_t len) {
+  cef_string_utf8_t* p = calloc(1, sizeof(cef_string_utf8_t));
+  p->str = str;
+  p->length = len;
+  return p;
+}
+
+cef_string_utf16_t* gocef_to_utf16_impl(char* str, size_t len) {
+  cef_string_utf16_t* p = calloc(1, sizeof(cef_string_utf16_t));
+  cef_string_utf8_to_utf16(str, len, p);
+  return p;
+}
+
 */
 import "C"
 import "image/color"
