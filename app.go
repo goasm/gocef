fbe71d42351ae461e4784afb305533336f0214ae
skylerlee <skyler.ac.lee@gmail.com>
2019-09-09 00:52:06 +0800

    docs: add doc comments

--- app.go
+++ app.go
@@ -25,11 +25,14 @@ func fillMainArgs(args *C.cef_main_args_t) {
 	args.argv = &argv[0]
 }
 
+// ExecuteProcess executes a secondary process using the command-line arguments,
+// it will block until the process should exit and then return the exit code.
 func ExecuteProcess() int {
 	retval := C.cef_execute_process(mainArgs, nil, nil)
 	return int(retval)
 }
 
+// Initialize initializes the CEF browser process
 func Initialize() bool {
 	settings := Settings{}
 	settings.NoSandbox = true
@@ -38,14 +41,17 @@ func Initialize() bool {
 	return gocefToBool(retval)
 }
 
+// RunMessageLoop runs the CEF message loop
 func RunMessageLoop() {
 	C.cef_run_message_loop()
 }
 
+// QuitMessageLoop quits the CEF message loop that was started by RunMessageLoop
 func QuitMessageLoop() {
 	C.cef_quit_message_loop()
 }
 
+// Shutdown shuts down the CEF browser process
 func Shutdown() {
 	C.cef_shutdown()
 }
