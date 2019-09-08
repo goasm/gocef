435cbcb00651ebe76dc3221fea4ec630c14e7d56
skylerlee <skyler.ac.lee@gmail.com>
2019-09-08 21:57:09 +0800

    feat: update LS handler

--- client.go
+++ client.go
@@ -6,22 +6,28 @@ package gocef
 extern cef_life_span_handler_t* gocef_client_get_life_span_handler(cef_client_t*);
 */
 import "C"
+import (
+	"unsafe"
+)
 
 type Client interface {
 	GetLifeSpanHandler() LifeSpanHandler
 }
 
-type clientDelegate struct {
-	client Client
-}
-
 //export gocef_client_get_life_span_handler
 func gocef_client_get_life_span_handler(c *C.cef_client_t) *C.cef_life_span_handler_t {
-	return nil
+	self := gocefGetRef(unsafe.Pointer(c)).(Client)
+	handler := self.GetLifeSpanHandler()
+	return lifeSpanHandlerDelegate{handler}.toNative()
+}
+
+type clientDelegate struct {
+	self Client
 }
 
 func (c clientDelegate) toNative() *C.cef_client_t {
 	p := (*C.cef_client_t)(gocefNew(C.sizeof_cef_client_t))
+	gocefSetRef(unsafe.Pointer(p), c.self)
 	p.get_life_span_handler = gocefToFuncPtr(C.gocef_client_get_life_span_handler)
 	return p
 }
--- life_span_handler.go
+++ life_span_handler.go
@@ -1,5 +1,19 @@
 package gocef
 
+/*
+#include "include/capi/cef_life_span_handler_capi.h"
+*/
+import "C"
+
 type LifeSpanHandler interface {
 	OnBeforeClose(b *Browser)
+}
+
+type lifeSpanHandlerDelegate struct {
+	self LifeSpanHandler
+}
+
+func (h lifeSpanHandlerDelegate) toNative() *C.cef_life_span_handler_t {
+	p := (*C.cef_life_span_handler_t)(gocefNew(C.sizeof_cef_life_span_handler_t))
+	return p
 }
