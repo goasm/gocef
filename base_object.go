c4ed4663c41713449ca3ac76f2e92d3d5ea4b935
skylerlee <skyler.ac.lee@gmail.com>
2019-09-08 20:54:04 +0800

    feat: add cgo reference mapping

--- base_object.go
+++ base_object.go
@@ -5,9 +5,35 @@ package gocef
 */
 import "C"
 import (
+	"errors"
 	"unsafe"
+)
+
+var (
+	objectRefMap  = make(map[uintptr]interface{})
+	errResolveRef = errors.New("failed to resolve reference")
 )
 
 func gocefNew(size int64) unsafe.Pointer {
 	return C.gocef_new_impl(C.ulong(size))
+}
+
+func gocefSetRef(ptr unsafe.Pointer, obj interface{}) {
+	objectRefMap[uintptr(ptr)] = obj
+}
+
+func gocefGetRef(ptr unsafe.Pointer) interface{} {
+	obj, ok := objectRefMap[uintptr(ptr)]
+	if !ok {
+		panic(errResolveRef)
+	}
+	return obj
+}
+
+func gocefDelRef(ptr unsafe.Pointer) {
+	_, ok := objectRefMap[uintptr(ptr)]
+	if !ok {
+		panic(errResolveRef)
+	}
+	delete(objectRefMap, uintptr(ptr))
 }
