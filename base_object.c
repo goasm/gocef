c093ea0394179dc2329976a4f30cb67b6b7759a0
skylerlee <skyler.ac.lee@gmail.com>
2019-09-14 16:33:56 +0800

    feat: implement ref count

--- base_object.c
+++ base_object.c
@@ -3,16 +3,36 @@
 #include <stdlib.h>
 #include "include/capi/cef_base_capi.h"
 
+typedef struct _ref_counted_impl_t {
+  size_t count;
+} ref_counted_impl_t;
+
+static inline ref_counted_impl_t* ref_counted(cef_base_ref_counted_t* base) {
+  uint8_t* p = (uint8_t*)base;
+  return (ref_counted_impl_t*)(p + base->size);
+}
+
 static void add_ref_impl(cef_base_ref_counted_t* self) {
+  ref_counted_impl_t* refcnt = ref_counted(self);
+  refcnt->count = refcnt->count + 1;
 }
 
 static int release_impl(cef_base_ref_counted_t* self) {
+  ref_counted_impl_t* refcnt = ref_counted(self);
+  refcnt->count = refcnt->count - 1;
+  if (refcnt->count == 0) {
+    free(self);
+  }
 }
 
 static int has_one_ref_impl(cef_base_ref_counted_t* self) {
+  ref_counted_impl_t* refcnt = ref_counted(self);
+  return refcnt->count == 1;
 }
 
 static int has_at_least_one_ref_impl(cef_base_ref_counted_t* self) {
+  ref_counted_impl_t* refcnt = ref_counted(self);
+  return refcnt->count >= 1;
 }
 
 void* gocef_new_impl(size_t size) {
@@ -23,6 +43,7 @@ void* gocef_new_impl(size_t size) {
   base->release = release_impl;
   base->has_one_ref = has_one_ref_impl;
   base->has_at_least_one_ref = has_at_least_one_ref_impl;
+  ref_counted(base)->count = 1;
   return obj;
 }
 
