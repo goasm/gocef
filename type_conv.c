b7c80bf258445413a8bff6bd0ef0b33796e13346
skylerlee <skyler.ac.lee@gmail.com>
2019-09-29 23:12:52 +0800

    fix: release string after conversion

--- type_conv.c
+++ type_conv.c
@@ -1,10 +1,13 @@
 #include "type_conv.h"
 
+#include <stdlib.h>
+
 void gocef_set_utf8_impl(cef_string_utf8_t* dst, char* str, size_t len) {
-  dst->str = str;
-  dst->length = len;
+  cef_string_utf8_set(str, len, dst, 1);
+  free(str);
 }
 
 void gocef_set_utf16_impl(cef_string_utf16_t* dst, char* str, size_t len) {
   cef_string_utf8_to_utf16(str, len, dst);
+  free(str);
 }
